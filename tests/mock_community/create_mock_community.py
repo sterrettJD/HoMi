abd7fe5ebec9f1f4aa4b1cad00cf687008620558
John Sterrett <sterrettJD@protonmail.com>
2023-08-17 08:32:45 -0600

    download genomes function

--- tests/mock_community/create_mock_community.py
+++ tests/mock_community/create_mock_community.py
@@ -1,7 +1,15 @@
 import subprocess
+import os
+
+def download_genomes(path="mock_community/data"):
+    subprocess.run(["cd", path])
+    subprocess.run(["bash", "tests/download_genomes_for_mock.sh"])
+
 
 def main():
-    subprocess.run(["bash", "tests/download_genomes_for_mock.sh"])
+    if os.path.exists("mock_community/data")==F:
+        download_genomes(path="mock_community/data")
+    # TODO: add a check to make sure it has the right stuff
 
 if __name__=="__main__":
     main()

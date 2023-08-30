16bcc370fed2b529ae88b8829993f9815cd37b73
John Sterrett <sterrettJD@protonmail.com>
2023-08-30 09:37:25 -0600

    add snakefile path getting

--- HoMi/HoMi.py
+++ HoMi/HoMi.py
@@ -1,10 +1,26 @@
 import subprocess
+import os
+
+# def get_args
+
+def get_snakefile_path():
+    path_2_script = os.path.dirname(__file__)
+    snakepath = os.path.join(path_2_script, "../snakefile")
+    # remove ".."
+    return os.path.normpath(snakepath)
+
+
 def main():
+    snakepath = get_snakefile_path()
     subprocess.run(["snakemake", 
+                    "-s", snakepath,
                     "--configfile", "tests/example_config.yaml", 
                     "-c1", 
                     "--latency-wait", "10", 
-                    "--rerun-incomplete", "--use-conda"])
+                    "--rerun-incomplete", 
+                    "--use-conda"])
 
 if __name__=="__main__":
     main()
+
+# https://stackoverflow.com/questions/74260602/how-to-run-a-python-script-relative-to-its-location

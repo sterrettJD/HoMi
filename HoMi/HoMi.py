cf6c6770dfdb8838b9642e40b4ffec3509cf6544
John Sterrett <sterrettJD@protonmail.com>
2023-08-30 14:22:53 -0600

    add argparse with config path

--- HoMi/HoMi.py
+++ HoMi/HoMi.py
@@ -1,7 +1,13 @@
 import subprocess
+import argparse
 import os
 
 # def get_args
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", help="path config file for pipeline (yaml)")
+    return parser.parse_args()
+
 
 def get_snakefile_path():
     path_2_script = os.path.dirname(__file__)
@@ -12,9 +18,12 @@ def get_snakefile_path():
 
 def main():
     snakepath = get_snakefile_path()
+
+    args = get_args()
+
     subprocess.run(["snakemake", 
                     "-s", snakepath,
-                    "--configfile", "tests/example_config.yaml", 
+                    "--configfile", args.config, 
                     "-c1", 
                     "--latency-wait", "10", 
                     "--rerun-incomplete", 

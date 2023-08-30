9e5fe771762ef9576afb8163240e5240348b77a1
John Sterrett <sterrettJD@protonmail.com>
2023-08-30 15:04:07 -0600

    add more detailed command generation

--- HoMi/HoMi.py
+++ HoMi/HoMi.py
@@ -6,6 +6,9 @@ import os
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("config", help="path config file for pipeline (yaml)")
+    parser.add_argument("profile", 
+                        help="path to a snakemake profile, such as a profile to submit jobs to SLURM",
+                        default=None)
     return parser.parse_args()
 
 
@@ -15,21 +18,27 @@ def get_snakefile_path():
     # remove ".."
     return os.path.normpath(snakepath)
 
+def construct_snakemake_command(snakepath, args):
+    command = ["snakemake", 
+                "-s", snakepath,
+                "--configfile", args.config, 
+                "--cores", 1, 
+                "--latency-wait", "10", 
+                "--rerun-incomplete", 
+                "--use-conda"]
+    
+    if args.profile is not None:
+        command = command.append("--profile", args.profile)
+
+    return command
+
 
 def main():
     snakepath = get_snakefile_path()
-
     args = get_args()
 
-    subprocess.run(["snakemake", 
-                    "-s", snakepath,
-                    "--configfile", args.config, 
-                    "-c1", 
-                    "--latency-wait", "10", 
-                    "--rerun-incomplete", 
-                    "--use-conda"])
+    command = construct_snakemake_command(snakepath, args)
+    subprocess.run(command)
 
 if __name__=="__main__":
     main()
-
-# https://stackoverflow.com/questions/74260602/how-to-run-a-python-script-relative-to-its-location

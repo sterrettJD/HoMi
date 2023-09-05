e534df86df5b5ae72a1a7ebe1b17f47a4104093c
John Sterrett <sterrettJD@protonmail.com>
2023-09-05 15:40:39 -0600

    add adapters path function

--- src/snake_utils.py
+++ src/snake_utils.py
@@ -5,3 +5,14 @@ def hostile_db_to_path(HOSTILE_DB, parent):
         return path.join(parent, HOSTILE_DB)
     else:
         return HOSTILE_DB
+    
+def get_adapters_path():
+    path_2_script = path.dirname(__file__)
+    adapters_path = path.join(path_2_script, "..", "data", "adapters.fa")
+    # remove ".."
+    adapters_path = path.normpath(adapters_path)
+
+    if path.exists(adapters_path):
+        return adapters_path
+    else:
+        raise FileNotFoundError(f"Adapters were not found in {adapters_path}.")

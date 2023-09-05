eaf46dc9a08e2c6e85bcb11c0425ded246985efa
John Sterrett <sterrettJD@protonmail.com>
2023-09-05 10:43:31 -0600

    add parent dir to download path

--- HoMi/snake_utils.py
+++ HoMi/snake_utils.py
@@ -1,7 +1,7 @@
 from os import path
 
-def hostile_db_to_path(HOSTILE_DB):
+def hostile_db_to_path(HOSTILE_DB, parent):
     if HOSTILE_DB in ["human-t2t-hla-argos985", "human-t2t-hla"] :
-        return path.join("data", HOSTILE_DB)
+        return path.join(parent, HOSTILE_DB)
     else:
         return HOSTILE_DB

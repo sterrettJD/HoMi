08ac90f21f12952e5dc0222cc109a75ffd6b422a
John Sterrett <sterrettJD@protonmail.com>
2023-11-03 14:29:46 -0600

    add clean humann temps

--- src/HoMi_cleanup.py
+++ src/HoMi_cleanup.py
@@ -9,6 +9,8 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("config", help="path config file for pipeline (yaml)")
     parser.add_argument("metadata", help="the metadata file used for HoMi")
+    parser.add_argument("--humann", action="store_true",
+                        help="Pass this to clean up humann unneeded files")
     return parser.parse_args()
 
 
@@ -22,12 +24,34 @@ def read_sample_list(filepath):
     return pd.read_csv(filepath)["Sample"]
 
 
+def clean_humann_temps(config, samples):
+    prefix = f"{config['PROJ']}.f{config['trim_fwd']}.{config['trunc_fwd']}.r{config['trim_rev']}.{config['trunc_rev']}"
+    humann_path = f"{prefix}.nonhost.humann"
+    temp_dirs = [os.path.join(humann_path, sample, f"{sample}_humann_temp") for sample in samples]
+    
+    for i, sample in enumerate(samples):
+        diamond_aligned = os.path.join(temp_dirs[i], f"{sample}_diamond_aligned.tsv")
+        diamond_unaligned = os.path.join(temp_dirs[i], f"{sample}_diamond_unaligned.fa")
+        print(f"Removing old file {diamond_aligned}")
+        os.remove(diamond_aligned)
+        print(f"Removing old file {diamond_unaligned}")
+        os.remove(diamond_unaligned)
+
+        remaining = os.listdir()
+        other_temps = [x.startswith("tmp") for x in remaining]
+
+        for dir in other_temps:
+            print(f"Removing temporary directory {dir}")
+            rmtree(dir)
+
+    
 def main():
     args = get_args()
     config = read_config(args.config)
     samples = read_sample_list(args.metadata)
 
-
+    if args.humann is not None:
+        clean_humann_temps(config, samples)
 
 
 

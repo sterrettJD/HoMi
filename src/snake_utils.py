08c99c20ae7169d02b507aade0ca7501e08baf07
John Sterrett <sterrettJD@protonmail.com>
2023-11-09 14:55:49 -0700

    code style things

--- src/snake_utils.py
+++ src/snake_utils.py
@@ -5,7 +5,8 @@ def hostile_db_to_path(HOSTILE_DB, parent):
         return path.join(parent, HOSTILE_DB)
     else:
         return HOSTILE_DB
-    
+
+
 def get_adapters_path():
     path_2_script = path.dirname(__file__)
     adapters_path = path.join(path_2_script, "..", "data", "adapters.fa")
@@ -16,7 +17,8 @@ def get_adapters_path():
         return adapters_path
     else:
         raise FileNotFoundError(f"Adapters were not found in {adapters_path}.")
-    
+
+
 def get_nonpareil_rmd_path():
     path_2_script = path.dirname(__file__)
     rmd_path = path.join(path_2_script, "rule_utils", "nonpareil_curves.Rmd")
@@ -28,6 +30,7 @@ def get_nonpareil_rmd_path():
     else:
         raise FileNotFoundError(f"Nonpareil Rmd was not found at {rmd_path}.")
 
+
 def get_nonpareil_html_path():
     path_2_script = path.dirname(__file__)
     html_path = path.join(path_2_script, "rule_utils", "nonpareil_curves.html")
@@ -35,7 +38,8 @@ def get_nonpareil_html_path():
     html_path = path.normpath(html_path)
 
     return html_path
-    
+
+
 def get_agg_script_path():
     path_2_script = path.dirname(__file__)
     agg_path = path.join(path_2_script, "rule_utils", "aggregate_metaphlan_bugslists.py")
@@ -46,7 +50,8 @@ def get_agg_script_path():
         return agg_path
     else:
         raise FileNotFoundError(f"Aggregation script was not found at {agg_path}.")
-    
+
+
 def get_mphlan_conv_script_path():
     path_2_script = path.dirname(__file__)
     conv_path = path.join(path_2_script, "rule_utils", "convert_mphlan_v4_to_v3.py")
@@ -57,7 +62,8 @@ def get_mphlan_conv_script_path():
         return conv_path
     else:
         raise FileNotFoundError(f"MetaPhlan conversion script was not found at {conv_path}.")
-    
+
+
 def get_taxa_barplot_rmd_path():
     path_2_script = path.dirname(__file__)
     rmd_path = path.join(path_2_script, "rule_utils", "Metaphlan_microshades.Rmd")
@@ -68,7 +74,8 @@ def get_taxa_barplot_rmd_path():
         return rmd_path
     else:
         raise FileNotFoundError(f"Taxa Barplot Rmd was not found at {rmd_path}.")
-    
+
+
 def get_func_barplot_rmd_path():
     path_2_script = path.dirname(__file__)
     rmd_path = path.join(path_2_script, "rule_utils", "HUMAnN_microshades.Rmd")
@@ -79,7 +86,8 @@ def get_func_barplot_rmd_path():
         return rmd_path
     else:
         raise FileNotFoundError(f"Functional Barplot Rmd was not found at {rmd_path}.")
-    
+
+
 def get_gmm_rmd_path():
     path_2_script = path.dirname(__file__)
     rmd_path = path.join(path_2_script, "rule_utils", "Gut_metabolic_modules.Rmd")
@@ -91,6 +99,7 @@ def get_gmm_rmd_path():
     else:
         raise FileNotFoundError(f"Gut metabolic modules Rmd was not found at {rmd_path}.")
 
+
 def get_sam2bam_path():
     path_2_script = path.dirname(__file__)
     sam2bam_path = path.join(path_2_script, "rule_utils", "sam2bam.sh")
@@ -101,6 +110,7 @@ def get_sam2bam_path():
         return sam2bam_path
     else:
         raise FileNotFoundError(f"Sam2bam script was not found at {sam2bam_path}.")
+
 
 def get_partition(default, config, rule_name):
     # First, check the config for a rule-specific partition
@@ -117,11 +127,13 @@ def get_partition(default, config, rule_name):
     # If there's nothing in the config, use what's in the snakefile
     return default
 
+
 def get_mem(default, config, rule_name):
     config_param = config.get(f"{rule_name}_mem_mb")
     if config_param is not None:
         return int(config_param)
     return default
+
 
 def get_runtime(default, config, rule_name):
     config_param = config.get(f"{rule_name}_runtime")

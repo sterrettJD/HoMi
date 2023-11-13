57729619ba1de30a80a185871fa575c8e2504bf5
John Sterrett <sterrettJD@protonmail.com>
2023-11-13 15:52:51 -0700

    add test cleanup works for diamond aligned

--- tests/test_HoMi_cleanup.py
+++ tests/test_HoMi_cleanup.py
@@ -1,8 +1,14 @@
 import pytest
 import pandas as pd
 import os
+from pathlib import Path
 
 import src.HoMi_cleanup as hmc
+
+# lightweight helper for making files
+#def touch(fname):
+#    with open(fname, 'w'):
+#        pass
 
 
 def test_config_read_correctly(filepath="tests/test_data/test_cleanup_config.yaml"):
@@ -20,3 +26,28 @@ def test_config_read_correctly(filepath="tests/test_data/test_cleanup_config.yam
         "trunc_fwd": 0,
         "trunc_rev": 0
         }
+    
+
+def test_deletes_correct_file(tmpdir, filepath="tests/test_data/test_cleanup_config.yaml"):
+    sample = "sample_1"
+    config = hmc.read_config(filepath)
+    prefix = f"{config['PROJ']}.f{config['trim_fwd']}.{config['trunc_fwd']}.r{config['trim_rev']}.{config['trunc_rev']}"
+    humann_path = os.path.join(tmpdir, f"{prefix}.nonhost.humann")
+    sample_path = os.path.join(humann_path, sample)
+    sample_temp_path = os.path.join(sample_path, f"{sample}_humann_temp")
+
+    diamond_aligned = os.path.join(sample_temp_path, f"{sample}_diamond_aligned.tsv")
+    Path(diamond_aligned).parent.mkdir(parents=True, exist_ok=True)
+    Path(diamond_aligned).touch()
+    
+    # Test that file is here
+    assert os.listdir(sample_temp_path)==[f"{sample}_diamond_aligned.tsv"]
+    # Move to temp dir
+    start_dir = os.getcwd()
+    os.chdir(tmpdir)
+    # should delete the files
+    hmc.clean_humann_temps(config, samples=[sample])
+    # Test that file is not be here
+    assert os.listdir(sample_temp_path)==[]
+    # Go back to start
+    os.chdir(start_dir)

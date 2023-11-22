b901ba732c3dbe7563a26b0c0b843d9040cb9115
John Sterrett <sterrettJD@protonmail.com>
2023-11-22 13:59:19 -0700

    add check for ntasks updater

--- tests/test_profile_setup.py
+++ tests/test_profile_setup.py
@@ -24,3 +24,26 @@ def test_check_conda(tmpdir):
     
     with pytest.raises(ValueError):
         ps.check_use_conda_slurm(tmpdir)
+
+
+def test_ntasks_mod(tmpdir):
+    cookiecutter("gh:Snakemake-Profiles/slurm", 
+                 output_dir=tmpdir, 
+                 no_input=True)
+    
+    slurm_utils_path = os.path.join(tmpdir, "slurm", "slurm_utils.py")
+    with open(slurm_utils_path, 'r') as file:
+        content = file.read()
+    
+    # check it's there by default
+    assert "options[\"cpus-per-task\"] = job_properties[\"threads\"]" in content
+
+    # This function should replace the cpus-per-task with ntasks
+    ps.convert_slurm_profile_tasks(tmpdir)
+
+    with open(slurm_utils_path, 'r') as file:
+        content = file.read()
+    
+    # check it's been updated
+    assert "options[\"ntasks\"] = job_properties[\"threads\"]" in content
+    assert "options[\"cpus-per-task\"] = job_properties[\"threads\"]" not in content

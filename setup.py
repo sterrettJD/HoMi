30661298ac119491c1e7d836dc389f2aebf5565b
John Sterrett <sterrettJD@protonmail.com>
2023-11-22 15:07:33 -0700

    add pytest requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
       version=__version__,
       python_requires='<3.12.0', # snakemake f-strings break in 3.12 (as of Nov 15, 2023)
       setup_requires=['pytest-runner'],
-      tests_require=['pytest'],
+      tests_require=['pytest', 'cookiecutter', 'pandas'],
       install_requires=['snakemake', 'pandas', 'biopython', 'numpy', 'argparse', 'pyyaml', 'cookiecutter'],
       scripts=['src/HoMi.py', 'src/snake_utils.py', 'src/HoMi_cleanup.py', 'src/profile_setup.py'],
       packages=find_packages(),

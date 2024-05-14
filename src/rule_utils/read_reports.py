5b79ce90c5044a8f7cd3a8091e4e9ba79bcc51e5
John Sterrett <sterrettJD@protonmail.com>
2024-05-14 14:10:12 -0600

    create df from multiple hostile reports

--- src/rule_utils/read_reports.py
+++ src/rule_utils/read_reports.py
@@ -1,4 +1,6 @@
 import json
+import pandas as pd
+from os import path
 
 def read_hostile_report(filepath) -> dict:
     with open(filepath, 'r') as file:
@@ -16,6 +18,19 @@ def get_nonhost_reads(hostile_dict) -> int:
 
 def get_percent_host(hostile_dict) -> float:
     return hostile_dict["reads_removed_proportion"]
+
+
+def create_df_from_hostile_reports(metadata, hostile_directory, sample_col="Sample") -> pd.DataFrame:
+    df = pd.DataFrame(index=metadata[sample_col].to_list(), 
+                      columns=["Reads passing QC", "Nonhost reads", "Percent host"])
+
+    for sample in metadata[sample_col].to_list():
+        report = read_hostile_report(path.join(hostile_directory, f"{sample}.report"))
+        df.loc[sample, ["Reads passing QC", "Nonhost reads", "Percent host"]] = [get_reads_into_hostile(report),
+                                                                                   get_nonhost_reads(report),
+                                                                                   get_percent_host(report)]
+    
+    return df
 
 
 def main():

1a4feda073916c3a536166afc6ddb4d907c5448d
John Sterrett <sterrettJD@protonmail.com>
2024-05-14 10:35:34 -0600

    test grabbers

--- tests/test_read_reports.py
+++ tests/test_read_reports.py
@@ -4,4 +4,21 @@ from src.rule_utils import read_reports as rr
 
 def test_read_hostile():
     report = rr.read_hostile_report("tests/test_data/hostile_report.report")
-    assert report["reads_out"] == 59944
+    assert report["version"] == "1.1.0"
+
+
+@pytest.fixture
+def report():
+    return rr.read_hostile_report("tests/test_data/hostile_report.report")
+
+
+def test_hostile_reads_in(report):
+    assert rr.get_reads_into_hostile(report) == 177014
+
+
+def test_hostile_reads_nonhost(report):
+    assert rr.get_nonhost_reads(report) == 59944
+
+
+def test_hostile_percent_host(report):
+    assert rr.get_percent_host(report) == 0.66136

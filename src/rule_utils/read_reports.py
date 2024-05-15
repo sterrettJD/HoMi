6c94b3a46409240978d70a195f39cb1c07d9f133
John Sterrett <sterrettJD@protonmail.com>
2024-05-15 12:55:29 -0600

    add original counts to df func

--- src/rule_utils/read_reports.py
+++ src/rule_utils/read_reports.py
@@ -3,19 +3,6 @@ import pandas as pd
 import subprocess
 from os import path
 
-def count_reads_fastq_gz(file_path) -> int:
-    try:
-        result = subprocess.run(f"gunzip -c {file_path} | wc -l", shell=True, capture_output=True, text=True, check=True)
-        line_count = int(result.stdout.strip())
-    
-        # Divide by 4 to get the number of reads (assuming FASTQ format with 4 lines per read)
-        read_count = line_count // 4
-        return read_count
-    
-    except subprocess.CalledProcessError as e:
-        raise ValueError(f"Error: {e}")
-        return None
-
 
 def read_hostile_report(filepath) -> dict:
     with open(filepath, 'r') as file:
@@ -63,6 +50,35 @@ def add_unmapped_nonhost_to_hostile_reports(unmapped_nonhost: dict, hostile_repo
     # but I think that get_unmapped_nonhost_from_humann() returning a dict makes more sense
     # as a standalone func that may be used for other things
     return hostile_reports_df.join(pd.Series(unmapped_nonhost, name="Unmapped nonhost RPK"))
+
+
+def count_reads_fastq_gz(file_path) -> int:
+    try:
+        result = subprocess.run(f"gunzip -c {file_path} | wc -l", shell=True, capture_output=True, text=True, check=True)
+        line_count = int(result.stdout.strip())
+    
+        # Divide by 4 to get the number of reads (assuming FASTQ format with 4 lines per read)
+        read_count = line_count // 4
+        return read_count
+    
+    except subprocess.CalledProcessError as e:
+        raise ValueError(f"Error: {e}")
+        return None
+
+
+def add_original_read_counts_to_df(metadata, directory, df,
+                                   sample_col="Sample", 
+                                   forward_extension="_R1.fq.gz",
+                                   reverse_extension="_R2.fq.gz") -> pd.DataFrame:
+    reads_per_sample = dict()
+    for sample in metadata[sample_col].to_list():
+        forward_path = path.join(directory, f"{sample}{forward_extension}")
+        reverse_path = path.join(directory, f"{sample}{reverse_extension}")
+        reads = count_reads_fastq_gz(forward_path) + count_reads_fastq_gz(reverse_path)
+        reads_per_sample[sample] = reads
+
+    reads_per_sample_series = pd.Series(reads_per_sample, name="Raw reads")
+    return df.join(reads_per_sample_series)
 
 
 def main():

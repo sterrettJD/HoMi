aafae802046c5f889155db39017e63761d8fa240
John Sterrett <sterrettJD@protonmail.com>
2024-11-21 10:21:32 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 __author__ = 'sterrettJD'
-__version__ = '1.0.11'
+__version__ = '1.0.12'
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / 'README.md').read_text()

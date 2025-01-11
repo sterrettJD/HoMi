af6011d04455d2fc4d6e1271d2b96c1ebccc1b0f
John Sterrett <sterrettJD@protonmail.com>
2025-01-11 13:54:44 -0700

    add metaphlan compatibility

--- benchmarking/Compare_taxonomy.R
+++ benchmarking/Compare_taxonomy.R
@@ -83,6 +83,69 @@ filter_host_from_kraken_df <- function(feature.table, host.id="9606"){
 }
 
 
+read_metaphlan <- function(file){
+  # Need to specify header=TRUE here, which ensures that data.table uses the header.
+  # If this is removed, it will not be robust to numeric sample IDs
+  # which will trigger data table to not use this row as the column names
+  bugslist <- data.table::fread(file, header=TRUE) %>% as.data.frame()
+  rownames(bugslist) <- bugslist$`#clade_name`
+  bugslist <- bugslist %>% 
+    dplyr::select(-c(`#clade_name`,
+                     NCBI_tax_id,
+                     additional_species))
+  
+  # just get the lowest tax level. We can collapse again later
+  feature.table <- filter_mphlan_by_taxonomy_level(bugslist, level="max")
+  
+  # replace NA with 0
+  feature.table[is.na(feature.table)] <- 0
+  
+  bugs <- rownames(feature.table)
+  taxonomy.table <- metaphlan_names_to_tax_table(bugs)
+  rownames(taxonomy.table) <- bugs
+  
+  return(list(abundance=feature.table, tax=taxonomy.table))
+}
+
+
+# grabs a certain level of taxonomic resolution from the metaphlan output
+# defaults to the max level of resolution
+filter_mphlan_by_taxonomy_level <- function(df, level="max"){
+  tax.names <- rownames(df)
+  levels.contained <- stringr::str_count(tax.names, "\\|")
+  
+  if (level=="max"){
+    level <- max(levels.contained)
+  }
+  
+  to.return <- df[levels.contained==level,]
+  rownames(to.return) <- rownames(df)[levels.contained==level]
+  return (to.return)
+}
+
+
+# Converts metaphlan formatted taxonomy names to a taxonomy table for phyloseq
+# based on https://gist.github.com/lwaldron/512d1925a8102e921f05c5b25de7ec94
+metaphlan_names_to_tax_table <- function(bugs){
+  splitted <- strsplit(bugs, split="|", fixed=T)
+  # create empty taxonomy matrix
+  taxmat <- matrix(NA, 
+                   ncol=max(sapply(splitted, length)), 
+                   nrow=length(splitted))
+  colnames(taxmat) <- c("kingdom", "phylum", "class", "order", "family", "genus", "species", "strain")[1:ncol(taxmat)]
+  
+  # add split taxonomy to the matrix
+  for (i in 1:nrow(taxmat)){
+    tax.resolution <- length(splitted[[i]])
+    taxmat[i, 1:tax.resolution] <- splitted[[i]]
+  }
+  # remove the p__, f__, etc to indicate level
+  taxmat <- gsub("[a-z]__", "", taxmat)
+  
+  return(as.data.frame(taxmat))
+}
+
+
 calculate_relative_abundance <- function(feature.table){
   column.sums <- colSums(feature.table)
   return(sweep(feature.table, 2, column.sums, FUN = "/"))
@@ -142,43 +205,49 @@ main <- function(){
     # remove host reads
     feature.table <- filter_host_from_kraken_df(feature.table)
     feature.table <- calculate_relative_abundance(feature.table)
-    
-    # get feature abundances for the taxa of interest
-    # and transpose for plotting
-    # and clean up column names
-    genus.lvl <- t(get_important_abundances(feature.table, tax.table, 
-                                          "genus", genera)) %>%
-      as.data.frame()
-    colnames(genus.lvl) <- genus.lvl["genus",]
-    genus.lvl <- genus.lvl[rownames(genus.lvl)!="genus",]
-    
-    species.lvl <- t(get_important_abundances(feature.table, tax.table, 
-                                            "species", species)) %>%
-      as.data.frame()
-    colnames(species.lvl) <- species.lvl["species",]
-    species.lvl <- species.lvl[rownames(species.lvl)!="species",]
-    
-    # get host percent
-    genus.lvl$`Percent host` <- get_percent_host(rownames(genus.lvl))
-    species.lvl$`Percent host` <- get_percent_host(rownames(species.lvl))
-    
-    # plot and save genus level
-    genus.plot <- plot_data(genus.lvl)
-    
-    save.path <- file.path(opts$output_dir, 
-                           paste0(opts$taxonomy_method, "_",
-                                  "genus.pdf"))
-    ggsave(save.path, genus.plot)
-   
-    
-    # plot and save species level
-    species.plot <- plot_data(species.lvl)
-    save.path <- file.path(opts$output_dir, 
-                           paste0(opts$taxonomy_method, "_",
-                                  "species.pdf"))
-    ggsave(save.path, species.plot)
-    
   }
+  
+  if(opts$taxonomy_method=="metaphlan"){
+    feature.table <- calculate_relative_abundance(feature.table)
+    tax.table <- tax.table %>%
+      mutate_all(function(x){gsub("_", " ", x)})
+  }
+  
+  # get feature abundances for the taxa of interest
+  # and transpose for plotting
+  # and clean up column names
+  genus.lvl <- t(get_important_abundances(feature.table, tax.table, 
+                                          "genus", genera)) %>%
+    as.data.frame()
+  colnames(genus.lvl) <- genus.lvl["genus",]
+  genus.lvl <- genus.lvl[rownames(genus.lvl)!="genus",]
+  
+  species.lvl <- t(get_important_abundances(feature.table, tax.table, 
+                                            "species", species)) %>%
+    as.data.frame()
+  colnames(species.lvl) <- species.lvl["species",]
+  species.lvl <- species.lvl[rownames(species.lvl)!="species",]
+  
+  # get host percent
+  genus.lvl$`Percent host` <- get_percent_host(rownames(genus.lvl))
+  species.lvl$`Percent host` <- get_percent_host(rownames(species.lvl))
+  
+  # plot and save genus level
+  genus.plot <- plot_data(genus.lvl)
+  
+  save.path <- file.path(opts$output_dir, 
+                         paste0(opts$taxonomy_method, "_",
+                                "genus.pdf"))
+  ggsave(save.path, genus.plot)
+  
+  
+  # plot and save species level
+  species.plot <- plot_data(species.lvl)
+  save.path <- file.path(opts$output_dir, 
+                         paste0(opts$taxonomy_method, "_",
+                                "species.pdf"))
+  ggsave(save.path, species.plot)
+  
 }
 
 main()

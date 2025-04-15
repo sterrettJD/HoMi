4d88c43535bfbec02c46c8a0d0ae801aab297c24
John Sterrett <sterrettJD@protonmail.com>
2025-04-15 10:21:41 -0600

    add hisat2 support

--- benchmarking/create_decontam_ref_human.py
+++ benchmarking/create_decontam_ref_human.py
@@ -3,7 +3,9 @@ import argparse
 import subprocess
 
 def get_args():
-    parser = argparse.ArgumentParser(description="Builds a big Bowtie2 reference for human RNA decontamination.")
+    parser = argparse.ArgumentParser(description="Builds a reference for human RNA decontamination.")
+    parser.add_argument("-m", "--method", type=str, default="bowtie2",
+                        help="Index method. Choose from bowtie2 (default) or hisat2.")
     parser.add_argument("-o", "--output_dir", type=str, default="host_reference",
                         help="Output directory to store the reference files (default: host_reference)")
     args = parser.parse_args()
@@ -54,18 +56,39 @@ def build_bowtie2_index(concatenated_fasta_path, bowtie2_index_prefix):
     print(f"Bowtie2 index built successfully at: {bowtie2_index_prefix}")
 
 
+
+def build_hisat2_index(concatenated_fasta_path, hisat2_index_prefix):
+    """
+    Build the HISAT2 index from the concatenated FASTA file.
+    """
+    print("Building HISAT2 index...")
+    subprocess.run(["hisat2-build", "--threads", "4",
+                    concatenated_fasta_path, hisat2_index_prefix], check=True)
+    print(f"HISAT2 index built successfully at: {hisat2_index_prefix}")
+
+
 def main():
     args = get_args()
     output_dir = args.output_dir
 
     # Define URLs for the reference sequences
-    REFERENCES = {
-        "CHM13v2.0_transcriptome": "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/009/914/755/GCF_009914755.1_T2T-CHM13v2.0/GCF_009914755.1_T2T-CHM13v2.0_rna.fna.gz",
-        "HLA": "ftp://ftp.ebi.ac.uk/pub/databases/ipd/imgt/hla/hla_nuc.fasta"
-    }
+    # Bowtie2 uses an RNA-based index
+    # HISAT2 uses a DNA-based index, since it accounts for alternative splicing
+    if args.method == "bowtie2":
+        REFERENCES = {
+            "CHM13v2.0_transcriptome": "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/009/914/755/GCF_009914755.1_T2T-CHM13v2.0/GCF_009914755.1_T2T-CHM13v2.0_rna.fna.gz",
+            "HLA": "ftp://ftp.ebi.ac.uk/pub/databases/ipd/imgt/hla/hla_nuc.fasta"
+        }
+    elif args.method =="hisat2":
+        REFERENCES = {
+            "CHM13v2.0_genome": "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/009/914/755/GCF_009914755.1_T2T-CHM13v2.0/GCF_009914755.1_T2T-CHM13v2.0_genomic.fna.gz",
+            "HLA": "ftp://ftp.ebi.ac.uk/pub/databases/ipd/imgt/hla/hla_nuc.fasta"
+        }
+    else:
+        raise ValueError(f"Your method {args.method} is not supported. Please enter bowtie2 or hisat2.")
 
     concatenated_fasta_path = os.path.join(output_dir, "concatenated.fa")
-    bowtie2_index_prefix = os.path.join(output_dir, "index")
+    index_prefix = os.path.join(output_dir, "index")
 
     os.makedirs(output_dir, exist_ok=True)
     
@@ -74,7 +97,10 @@ def main():
 
 
     concatenate_fasta(REFERENCES, concatenated_fasta_path, output_dir)
-    build_bowtie2_index(concatenated_fasta_path, bowtie2_index_prefix)
+    if args.method == "bowtie2":
+        build_bowtie2_index(concatenated_fasta_path, index_prefix)
+    elif args.method == "hisat2":
+        build_hisat2_index(concatenated_fasta_path, index_prefix)
 
 
 if __name__ == "__main__":

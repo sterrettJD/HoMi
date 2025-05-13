1fac5ce99032c67a5de3f0f0cd1e9579469d759c
John Sterrett <sterrettJD@protonmail.com>
2025-05-13 10:25:56 -0600

    add index ext getter

--- src/homi_pipeline/snake_utils.py
+++ src/homi_pipeline/snake_utils.py
@@ -228,3 +228,16 @@ def get_hostile_aligner(config):
                      Please provide a valid Hostile aligner
                      ({', '.join(accepted_aligners)})
                      """)
+
+
+def get_hostile_exts(config):
+    aligner = get_hostile_aligner(config)
+    
+    if aligner == "hisat2":
+        exts = [".1.ht2", ".2.ht2", ".3.ht2", 
+                ".4.ht2", ".5.ht2", ".6.ht2",
+                ".7.ht2", ".8.ht2"]
+    elif aligner == "bowtie2":
+        exts = [".1.bt2", ".2.bt2", ".3.bt2", 
+                ".4.bt2", ".rev.1.bt2", ".rev.2.bt2"]
+    return exts
